e6ca2b07f7e5e123d744f647f434707088f0f812
Jonathan Beurel <bejonster@gmail.com>
2017-03-06 22:36:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ sudo apt-get install git
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install ansible -y
